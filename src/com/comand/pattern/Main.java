6505ba9ae0bd8658e7a0d3c17d893b075bee08f7
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2022-03-19 00:33:35 +0200

    adding

--- src/com/comand/pattern/Main.java
+++ src/com/comand/pattern/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
 
