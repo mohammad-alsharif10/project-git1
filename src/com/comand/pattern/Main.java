0c03ff038928aab305490fe215d76c54ada5750e
mohammad alshsrif <mohammad-alsharif10@outlook.com>
2022-03-19 20:16:45 +0200

    adding new file

--- src/com/comand/pattern/Main.java
+++ src/com/comand/pattern/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("hello");
-        System.out.print("hello");
-        System.out.print("hello");
-        System.out.print("hello");   System.out.print("hello");
+        System.out.println("hello x");
+        System.out.println("helloxx");
+        System.out.println("hellozz");
 
         System.out.println("hello");
         System.out.println("hello");
